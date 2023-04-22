db23dc10210b6a46317b22e6823481df08bf7fc2
Mustufa-kerawala <mustufakerawala@outlook.com>
2023-04-22 17:08:48 +0300

    Completed Repo

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,11 +6,36 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const newFoods = [...foods, newFood];
+    setFoods(newFoods);
   }
 
+  function handleLiClick(id) {
+    // Logic to remove the food from the list
+    // const newFoods = foods.filter((food) => food.id !== id);
+    // setFoods(newFoods);
+
+    // Logic to increase the heat level of the food
+    const newFoods = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      }
+      else{
+        return food;
+      }
+    });
+    setFoods(newFoods);
+    
+    
+  }
+
+  // Creating a functiong to handle the click event and increase heat level
+
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() => handleLiClick(food.id)} >
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
